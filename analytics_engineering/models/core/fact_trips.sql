5713acff788f830db8b3ea13e475831138604a17
Roman Didyk <rd.data.engineering@gmail.com>
2024-02-21 20:07:20 +0000

    fixed

--- analytics_engineering/models/core/fact_trips.sql
+++ analytics_engineering/models/core/fact_trips.sql
@@ -35,8 +35,8 @@ trips_unioned.tripid,
     trips_unioned.service_type,
     trips_unioned.rate_code, 
     trips_unioned.pickup_locationid,
-    dropoff_zone.borough as pickup_borough, 
-    dropoff_zone.zone as pickup_zone, 
+    pickup_zone.borough as pickup_borough, 
+    pickup_zone.zone as pickup_zone, 
     trips_unioned.dropoff_locationid,
     dropoff_zone.borough as dropoff_borough, 
     dropoff_zone.zone as dropoff_zone,  
@@ -58,5 +58,7 @@ trips_unioned.tripid,
     -- , 
     -- trips_unioned.payment_type_description
 from trips_unioned
-inner join dim_zones as dropoff_zone
+inner join dim_zones as pickup_zone
 on trips_unioned.pickup_locationid=dropoff_zone.locationid
+inner join dim_zones as dropoff_zone
+on trips_unioned.dropoff_locationid=dropoff_zone.locationid
